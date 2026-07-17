26fcc0885387b3bae5a25eb081240fb8ecfa140f
DarRomc <romerochagua.darwin2021@gmail.com>
2026-07-16 21:38:00 -0500

    Create exepciones.cpp

---
+++ PROGRAMACION I/UNIDAD IV/SEMANA 13/exepciones.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+using namespace std;
+
+int main() {
+    int divisor, dividendo;
+    float resultado;
+
+    cout << "Ingrese el dividendo: "; cin >> dividendo;
+    cout << "Ingrese el divisor: "; cin >> divisor;
+
+    try {
+        if (divisor == 0) {
+            throw runtime_error("Error al dividir por cero.");
+        }
+        resultado = static_cast<float>(dividendo) / divisor;
+        cout << "El resultado es: " << resultado << endl;
+    } catch (const runtime_error& e) {
+        cerr << e.what() << endl;
+    }
+
+    return 0;
+}
